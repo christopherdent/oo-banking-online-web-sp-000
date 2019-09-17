f34ab6e6479ea5dfbe42d7272fce5706cb3303c8
Christopher Dent <christopherdent01@gmail.com>
2019-09-17 23:47:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,7 +33,7 @@ class Transfer
     
     account = BankAccount.new(name)
     if self.valid? == false 
-      rejected 
+      p rejected 
       puts "Transaction rejected. Please check your account balance."
     end
     
