da2851f9b4b6beed229bb69aeff0213fa0417b7b
Christopher Dent <christopherdent01@gmail.com>
2019-09-18 00:07:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,27 +22,10 @@ class Transfer
   
   def execute_transaction 
   
-    self.status = "pending"
-    receiver.balance = receiver.balance += amount
-    sender.balance = sender.balance -= amount
-    self.status = "complete"
-   
-    if self.status = "complete"
-      @amount = 0
-    elsif 
+  if valid? && status == "pending"
+    if amount < sender.balance 
     
     
-    end 
-    
-    
-    account = BankAccount.new(name)
-    if self.valid? == false || nil 
-      "Transaction rejected. Please check your account balance."
-      @status = "rejected"
-    end
-    
- 
- @status 
   end 
   
   
